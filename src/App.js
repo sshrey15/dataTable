d7707cbf32caf697084e1022f4d2f945d28665ee
shrey <shreysingh152004@gmail.com>
2024-01-19 17:31:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const URL = "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001";
+
+  useEffect(() => {
+    fetchData();
+
+  }, []);
+
+  const fetchData = () =>{
+      fetch(URL)
+          .then(response => response.json())
+          .then(data => setData(data))
+          .catch(error => console.log(error))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee Data</h1>
+      <table>
+        <thead>
+        <tr>
+          <th>Id</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+            </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
